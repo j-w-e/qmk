6be69216dd0e9a8eee53343509260bd90d111e0a
Hugh Earp <hugh.earp@gmail.com>
2023-09-13 19:27:55 +0200

    adding rgb_matrix

--- wysteria/config.h
+++ wysteria/config.h
@@ -9,6 +9,15 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
 
+#ifdef RGB_MATRIX_ENABLE
+#    define SPLIT_TRANSPORT_MIRROR
+#    define RGB_MATRIX_LED_COUNT 36
+#    define RGB_MATRIX_SPLIT { 18, 18 }
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 50
+#    define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED
+#    define RGB_MATRIX_KEYPRESSES
+#endif
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
