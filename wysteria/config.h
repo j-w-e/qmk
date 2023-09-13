dd59561a800665a3b76e5e92483b6022da631f31
Hugh Earp <hugh.earp@gmail.com>
2023-09-13 18:41:12 +0200

    working default keymap, but split elements need to be tested better

---
+++ wysteria/config.h
@@ -0,0 +1,26 @@
+// Copyright 2023 j-w-e (@j-w-e)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define MASTER_RIGHT
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
---
+++ wysteria/rules.mk
@@ -0,0 +1,6 @@
+# RP2040-specific options
+ALLOW_WARNINGS = yes
+PICO_INTRINSICS_ENABLED = no # ATM Unsupported by ChibiOS.
+
+
+SERIAL_DRIVER = vendor
