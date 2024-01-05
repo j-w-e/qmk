a068294348f05bff4f909c02b1e04d1978601b25
Hugh Earp <hugh.earp@gmail.com>
2024-01-05 08:45:57 +0100

    minor tweaks to positions of leds

--- wysteria/wysteria.c
+++ wysteria/wysteria.c
@@ -28,7 +28,7 @@
  * ├────────────────────┤                 ├────────────────────┤
  *   15  14   9   8   3                     21  26  27  32  33
  * ╰────────────────────╯                 ╰────────────────────╯
- *                    0   1   2     18  19  20
+ *                    2   1   0     18  19  20
  *                 ╰────────────╯ ╰────────────╯
  */
 
@@ -42,46 +42,46 @@ led_config_t g_led_config = { {
     {      17,      12,      11,      6,     5,    0    }, // Top row
     {      16,      13,      10,      7,     4,    1    }, // Middle row
     {      15,      14,      9,       8,     3,    2    }, // Bottom row
-    {     NO_LED, NO_LED,     NO_LED,     NO_LED, NO_LED, NO_LED }, // I actually don't understand why this is necessary, but it works
+    {     NO_LED, NO_LED,     NO_LED,     NO_LED, NO_LED, NO_LED }, // Necessary because I've hotwired the rotary to be it's own column
     // Right split - final column is thumb cluster
     // columns are: pinky, ring, middle, index, inner, thumb, in that order
     // for thumb col, top row is reachy thumb, bottom row is tucky thumb
     {     35,       30,     29,     24,    23,   18    }, // Top row
     {     34,       31,     28,     25,    22,   19    }, // Middle row
     {     33,       32,     27,     26,    21,   20    }, // Bottom row
-    {     NO_LED, NO_LED,     NO_LED,     NO_LED, NO_LED, NO_LED }, // I actually don't understand why this is necessary, but it works
+    {     NO_LED, NO_LED,     NO_LED,     NO_LED, NO_LED, NO_LED }, // Necessary because I've hotwired the rotary to be it's own column
 }, {
     /* LED index to physical position. */
     // Left split.
-    /* index=0  */ {   0,  42 }, {   0,  21 }, {   0,   0 }, // col 1 (left most)
-    /* index=3  */ {  18,   0 }, {  18,  21 }, {  18,  42 }, // col 2
-    /* index=6  */ {  36,  42 }, {  36,  21 }, {  36,   0 },
-    /* index=9  */ {  54,   0 }, {  54,  21 }, {  54,  42 },
-    /* index=12 */ {  72,   0 }, {  72,  21 }, {  72,  42 },
-    /* index=15 */ {  72,  64 }, {  90,  64 }, { 108,  64 }, // Thumb cluster
+    /* index=0  */ {  99,  14 }, {  88,  24 }, {  76,  30 }, // thumb
+    /* index=3  */ {  82,  48 }, {  82,  67 }, {  82,  86 }, // inner index
+    /* index=6  */ {  70,  88 }, {  70,  69 }, {  70,  50 },
+    /* index=9  */ {  58,  60 }, {  58,  79 }, {  58,  98 },
+    /* index=12 */ {  46,  88 }, {  46,  69 }, {  46,  50 },
+    /* index=15 */ {  33,  39 }, {  33,  58 }, {  20,  62 }, // pinky
     // Right split.
-    /* index=18 */ { 224,  42 }, { 224,  21 }, { 224,   0 }, // col 10 (right most)
-    /* index=21 */ { 206,   0 }, { 206,  21 }, { 206,  42 }, // col 9
-    /* index=24 */ { 188,  42 }, { 188,  21 }, { 188,   0 },
-    /* index=27 */ { 170,   0 }, { 170,  21 }, { 170,  42 },
-    /* index=30 */ { 152,   0 }, { 152,  21 }, { 152,  42 },
-    /* index=33 */ { 152,  64 }, { 134,  64 }, { 116,  64 }, // Thumb cluster
+    /* index=18 */ { 148,  14 }, { 136,  24 }, { 125,  30 }, // thumb
+    /* index=21 */ { 142,  48 }, { 142,  67 }, { 142,  86 }, // inner index
+    /* index=24 */ { 154,  88 }, { 154,  69 }, { 154,  50 },
+    /* index=27 */ { 166,  60 }, { 166,  79 }, { 166,  98 },
+    /* index=30 */ { 178,  88 }, { 178,  69 }, { 178,  50 },
+    /* index=33 */ { 191,  39 }, { 191,  58 }, { 191,  62 }, // pinky
 }, {
     /* LED index to flag. */
     // Left split.
-    /* index=0  */ LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, // col 1
-    /* index=3  */ LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, // col 2
+    /* index=0  */ LED_FLAG_MODIFIER, LED_FLAG_MODIFIER, LED_FLAG_MODIFIER, // thumb
+    /* index=3  */ LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, // inner index
     /* index=6  */ LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
     /* index=9  */ LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
     /* index=12 */ LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
-    /* index=15 */ LED_FLAG_MODIFIER, LED_FLAG_MODIFIER, LED_FLAG_MODIFIER, // Thumb cluster
+    /* index=15 */ LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, // pinky
     // Right split.
-    /* index=18 */ LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, // col 10
-    /* index=21 */ LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, // col 9
+    /* index=18 */ LED_FLAG_MODIFIER, LED_FLAG_MODIFIER, LED_FLAG_MODIFIER, // thumb
+    /* index=21 */ LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, // inner index
     /* index=24 */ LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
     /* index=27 */ LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
     /* index=30 */ LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
-    /* index=33 */ LED_FLAG_MODIFIER, LED_FLAG_MODIFIER, LED_FLAG_MODIFIER, // Thumb cluster
+    /* index=33 */ LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, // pinky
 } };
 #endif
 // clang-format on
